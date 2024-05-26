892c40bd3c4343000ee78764333384a6bc964635
Marcus <marcus.perry2017@gmail.com>
2024-05-26 05:01:29 -0400

    Fixing

--- aeroquest-app/aeroquest-backend/server.js
+++ aeroquest-app/aeroquest-backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+};
+app.use(cors( corsOptions ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
