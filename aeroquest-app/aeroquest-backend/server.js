32f05c07ffc021196132d3361acb7b06f2132c44
Marcus <marcus.perry2017@gmail.com>
2024-05-26 10:27:29 -0400

    fixing

--- aeroquest-app/aeroquest-backend/server.js
+++ aeroquest-app/aeroquest-backend/server.js
@@ -11,13 +11,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-};
-app.use(cors( corsOptions ));
+// const corsOptions = {
+//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+// };
+// app.use(cors( corsOptions ));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 // Database connection string
 const connectionString = process.env.DATABASE_URL;
